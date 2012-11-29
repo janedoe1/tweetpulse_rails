70ee2ecb54fb6d02fa88c24c7ac3c69ae7bf8636
Kyle Conarro <kyle.conarro@gmail.com>
2012-11-28 21:03:43 -0500

    adding stuff to dashboard

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -1,12 +1,13 @@
 class DashboardPresenter
 
-  attr_reader :tag, :graph_min, :graph_max
+  attr_reader :tag, :graph_min, :graph_max, :searches
 
   def initialize(current_user, options = {})
     @user = current_user
     @searches = current_user.searches
     @tweets = @searches.collect {|s| s.tweets}.flatten
     @sentiments = Sentiment.where("tweet_id in (?)", @tweets.map(&:id))
+    @twitter_users = @searches.collect {|s| s.twitter_users}.flatten
   end
   
   def sentiment_tweet_count(label)
@@ -17,6 +18,21 @@ class DashboardPresenter
     @searches.order("created_at DESC").limit(count).collect {|search| %(["#{search.label}", #{(search.tweets.count)}])}
   end
   
+  def top_influencers(count=5)
+    #TwitterUser.order("influence DESC").limit(count)
+    TwitterUser.order("influence DESC").limit(count).collect {|twitter_user| %(["@#{twitter_user.handle}", #{(twitter_user.influence)}])}
+  end
   
+  def influence_vs_outreach(count=10)
+    TwitterUser.order("influence DESC").limit(count).collect {|twitter_user| %([#{twitter_user.outreach}, #{(twitter_user.influence)}])}
+  end
+  
+  def top_tweets(count=5)
+    Tweet.find(:all, :select => 'tweets.*, count(retweets.id) as retweet_count',
+                 :joins => 'left outer join retweets on retweets.tweet_id = tweets.id',
+                 :group => 'tweets.id',
+                 :order => 'retweet_count ASC').take(count)
+    #@tweets.collect {|t| t.retweets.count }
+  end
   
 end
