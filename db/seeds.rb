d45fde2438159734ff599423fcc4642fa9a173dd
Kyle Conarro <kyle.conarro@gmail.com>
2012-12-02 17:51:08 -0500

    testing db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+u = User.new(
+  :first_name => "Kyle",
+  :last_name => "Conarro",
+  :email => "kyle.conarro@gmail.com",
+  :password => 'p4ssw0rd'
+)
+u.save!(:validate => false)
