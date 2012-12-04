4f4a8881f3f24bba91943724979f9fafaabac097
Kyle Conarro <kyle.conarro@gmail.com>
2012-12-04 03:08:26 -0500

    still trying to fix sentiment column issue...

--- db/migrate/20121204071819_change_tweet_id_to_integer_in_sentiments.rb
+++ db/migrate/20121204071819_change_tweet_id_to_integer_in_sentiments.rb
@@ -1,5 +1,11 @@
 class ChangeTweetIdToIntegerInSentiments < ActiveRecord::Migration
   def change
-    change_column :sentiments, :tweet_id, :integer
+    rename_column :sentiments, :tweet_id, :tweet_id_string
+    add_column :sentiments, :tweet_id, :integer
+
+    Sentiment.reset_column_information
+    Sentiment.find_each { |s| s.update_attribute(:tweet_id, s.tweet_id_string.to_i) } 
+    remove_column :sentiments, :tweet_id_string
   end
+
 end
