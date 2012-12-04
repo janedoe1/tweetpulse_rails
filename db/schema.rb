2a2626fbec22b78f5966cfe2991f6c200004eea6
Kyle Conarro <kyle.conarro@gmail.com>
2012-12-04 02:28:55 -0500

    changed tweet_id to int in sentiments, search form cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202212457) do
+ActiveRecord::Schema.define(:version => 20121204071819) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(:version => 20121202212457) do
   end
 
   create_table "sentiments", :force => true do |t|
-    t.string   "tweet_id"
+    t.integer  "tweet_id"
     t.string   "label"
     t.float    "negative"
     t.float    "positive"
